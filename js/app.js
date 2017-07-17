3f85beaca750eded939e4741e0701787bba42821
pt <403782827@qq.com>
2017-07-17 20:16:43 +0800

    数据展示完成

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 (function (angular) {
 	'use strict';
+    angular.module('todoApp',[])
+            .controller('TodoController',['$scope',todoHandler])
+    function todoHandler($scope){
+        // 1 展示任务列表
+        var arr=[
+            { id: 1, name: '吃饭', isCompleted: false },
+            { id: 2, name: '睡觉', isCompleted: true },
+            { id: 3, name: '打豆豆', isCompleted: false },
+            { id: 4, name: '豆豆是谁', isCompleted: false}
+        ]
+        this.arr=arr;
 
-	// Your starting point. Enjoy the ride!
-    
+
+    }
 
 })(angular);
